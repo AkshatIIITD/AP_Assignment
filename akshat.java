e2b0d12a5edaf4c19e3e9e61262ce3ae7c261857
Akshat <akshat20172@iiitd.ac.in>
2021-10-08 14:22:51 +0530

    added clsses and main function

--- akshat.java
+++ akshat.java
@@ -1,5 +1,54 @@
+import java.util.Scanner;
+
+
+class VaccineCentres {
+    
+}
+
+class Citizens {
+
+}
+
+class Slot {
+
+}
+
 public class akshat {
     public static void main(String[] args) {
-        System.out.println("myfile");
+        System.out.println("CoWin Portal initialized....");
+        System.out.println("---------------------------------");
+        System.out.println("1. Add Vaccine");
+        System.out.println("2. Register Hospital");
+        System.out.println("3. Register Citizen");
+        System.out.println("4. Add Slot for Vaccination");
+        System.out.println("5. Book Slot for Vaccination");
+        System.out.println("6. List all slots for a Hospital");
+        System.out.println("7. Check Vaccination Status");
+        System.out.println("8. Exit");
+        System.out.println("---------------------------------");
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            int queryNo = Integer.parseInt(in.nextLine());
+            if (queryNo == 1) {
+
+            } else if (queryNo == 2) {
+
+            } else if (queryNo == 3) {
+
+            } else if (queryNo == 4) {
+
+            } else if (queryNo == 5) {
+
+            } else if (queryNo == 6) {
+
+            } else if (queryNo == 7) {
+
+            } else if (queryNo == 8) {
+
+            } else {
+                System.out.println("Incorrect menu option");
+            }
+        }
+        in.close();
     }
 }
