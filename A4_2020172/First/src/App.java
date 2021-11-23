14b5984c03df5cf8013100377f23682f5a2bbef0
Akshat <akshat20172@iiitd.ac.in>
2021-11-23 10:10:55 +0530

    book class created

--- A4_2020172/First/src/App.java
+++ A4_2020172/First/src/App.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
+class Book {
+    final String bookName;
+    final String ISBN;
+    final String barCode;
+    
+    Book(String assignedBookName, String assignedISBN, String assignedBarCode) {
+        bookName = assignedBookName;
+        ISBN = assignedISBN;
+        barCode = assignedBarCode;
+    }
+}
+
+class Rack {
+    
+}
+
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+
     }
 }
