8ae34b211ff06adeec1dcd34fe94940451662bac
Akshat <akshat20172@iiitd.ac.in>
2021-11-25 02:43:39 +0530

    started 2

--- A4_2020172/Second/src/App.java
+++ A4_2020172/Second/src/App.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the number of rows of the matrix: ");
+        int m = Integer.parseInt(in.nextLine());
+        System.out.print("Enter the number of columns of the matrix: ");
+        int n = Integer.parseInt(in.nextLine());
+        int[][] matrix = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = in.nextInt();        
+            }
+        }
+        
+        in.close();        
     }
 }
