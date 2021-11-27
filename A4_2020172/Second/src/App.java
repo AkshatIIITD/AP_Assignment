4d18d17ccf603312254e6a382cacfab107c995c3
Akshat <akshat20172@iiitd.ac.in>
2021-11-27 16:02:06 +0530

    Basic structure created

--- A4_2020172/Second/src/App.java
+++ A4_2020172/Second/src/App.java
@@ -1,56 +1,137 @@
 import java.util.Scanner;
 
-class Functions <T>{
-    Scanner in = new Scanner(System.in);
-}
+class Functions <T> {
 
+    public T createMatrix (T newPixelSet) {
+        Scanner in = new Scanner(System.in);
+        if (newPixelSet instanceof ColorfulPixelSet) {
+            System.out.print("Enter the number of rows of the matrix: ");
+            int m = Integer.parseInt(in.nextLine());
+            System.out.print("Enter the number of columns of the matrix: ");
+            int n = Integer.parseInt(in.nextLine());
+            int [][][] matrix = new int[m][n][3];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    int[] colorset = new int[3];
+                    for (int k = 0; k < 3; k++) {
+                        colorset[k] = in.nextInt();
+                    }
+                    matrix[i][j] = colorset;
+                }
+            }
+            ((ColorfulPixelSet) newPixelSet).setColorMatrix(matrix);
 
+        } else {
+            System.out.print("Enter the number of rows of the matrix: ");
+            int m = Integer.parseInt(in.nextLine());
+            System.out.print("Enter the number of columns of the matrix: ");
+            int n = Integer.parseInt(in.nextLine());
+            int[][] matrix = new int[m][n];
 
-class ColorfulPixel {
-    int[] RGB = new int[3];;
-    ColorfulPixel(int redColor, int greenColor, int blueColor) {
-        RGB[0] = redColor;
-        RGB[1] = greenColor;
-        RGB[2] = blueColor;
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    matrix[i][j] = in.nextInt();        
+                }
+            }
+            ((GreyscalePixelSet) newPixelSet).setGreyMatrix(matrix);
+        }
+        in.close();
+        return newPixelSet;
+    }
 
+    public void printer(T newPixelSet) {
+        if (newPixelSet instanceof ColorfulPixelSet) {
+            int m = ((ColorfulPixelSet) newPixelSet).getRows();
+            int n = ((ColorfulPixelSet) newPixelSet).getColumns();
+            int [][][] matrix = ((ColorfulPixelSet) newPixelSet).getColorMatrix();
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    System.out.println(
+                        "[R: "+matrix[i][j][0]+
+                        ", G: "+matrix[i][j][1]+
+                        ", B: "+matrix[i][j][2]+"]"
+                    );                    
+                }
+            }
+        } else {
+            int m = ((GreyscalePixelSet) newPixelSet).getRows();
+            int n = ((GreyscalePixelSet) newPixelSet).getColumns();
+            int [][] matrix = ((GreyscalePixelSet) newPixelSet).getGreyMatrix();
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    System.out.println(matrix[i][j]);
+                }
+            }
+        }  
     }
 }
 
-class GreyscalePixel {
-    int greycolor;
-    GreyscalePixel(int greycolor) {
-        this.greycolor = greycolor;
+class ColorfulPixelSet {
+    int[][][] colorMatrix;
+
+    public void setColorMatrix(int[][][] colorMatrix) {
+        this.colorMatrix = colorMatrix;
+    }
+    
+    public int[][][] getColorMatrix() {
+        return colorMatrix;
+    }
+
+    public int getRows() {
+        return colorMatrix.length;
+    }
+
+    public int getColumns() {
+        return colorMatrix[0].length;
+    }
+}
+
+class GreyscalePixelSet {
+
+    int[][] greyMatrix;
+
+    public void setGreyMatrix(int[][] greyMatrix) {
+        this.greyMatrix = greyMatrix;
+    }
+    
+    public int[][] getGreyMatrix() {
+        return greyMatrix;
+    }
+
+    public int getRows() {
+        return greyMatrix.length;
+    }
+
+    public int getColumns() {
+        return greyMatrix[0].length;
     }
 } 
 
 
-public class App {
-    public static void main(String[] args) {
+public class App <T>{
+    public static <T> void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter the number of rows of the matrix: ");
-        int m = Integer.parseInt(in.nextLine());
-        System.out.print("Enter the number of columns of the matrix: ");
-        int n = Integer.parseInt(in.nextLine());
         System.out.println("Enter the type of image: ");
         System.out.println("1. Colorful");
         System.out.println("2. Greyscale");
         int type = Integer.parseInt(in.nextLine());
+        T newPixelSet;
+        
         if (type == 1) {
-
+            newPixelSet = (T) new ColorfulPixelSet();
+            Functions<ColorfulPixelSet> func = new Functions<>();
+            newPixelSet = (T) func.createMatrix((ColorfulPixelSet) newPixelSet);
+            func.printer((ColorfulPixelSet) newPixelSet);            
+        
         } else if (type == 2) {
+            newPixelSet = (T) new GreyscalePixelSet();
+            Functions<GreyscalePixelSet> func = new Functions<>();
+            newPixelSet = (T) func.createMatrix((GreyscalePixelSet) newPixelSet);
+            func.printer((GreyscalePixelSet) newPixelSet);
 
         } else {
             System.out.println("Wrong input!");
-        }
-        
-        int[][] matrix = new int[m][n];
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                matrix[i][j] = in.nextInt();        
-            }
-        }
-        
+        }       
         in.close();        
     }
 }
